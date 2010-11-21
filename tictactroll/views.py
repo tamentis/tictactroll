21647cb9caa9bf97414cab75ee488cd952d0a78c
Bertrand Janin <tamentis@neopulsar.org>
2010-11-21 02:28:53 -0500

    Added add_grid and welcome.

--- tictactroll/views.py
+++ tictactroll/views.py
@@ -1,2 +1,27 @@
-def my_view(request):
-    return {'project':'tictactroll'}
+from gamestate import GameState
+from pyramid.httpexceptions import HTTPFound, HTTPBadRequest
+
+def game(request):
+    if "gamestate" not in request.session:
+        return HTTPFound(location="/")
+
+    gamestate = request.session["gamestate"]
+    return { "gamestate": gamestate }
+
+def welcome(request):
+    return { "countdown": GameState.initial_countdown }
+
+def enter_game(request):
+    try:
+        username = request.params["username"]
+    except:
+        return HTTPBadRequest()
+
+    gamestate = GameState(username)
+    request.session["gamestate"] = gamestate
+    request.session.save()
+
+    return HTTPFound(location="/game")
+
+def add_grid(request):
+    return { "status": "much_success" }
